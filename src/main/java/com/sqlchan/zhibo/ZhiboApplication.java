2196766e2d30f752324c96900574beb84bbc92f1
a <sqlchan@163.com>
2017-05-17 20:02:55 +0800

    Initial commit

---
+++ src/main/java/com/sqlchan/zhibo/ZhiboApplication.java
@@ -0,0 +1,12 @@
+package com.sqlchan.zhibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhiboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhiboApplication.class, args);
+	}
+}
